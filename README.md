74b1156b941679641948bd07ea2dd32a68111fef
Hazem Kawas <hazem.k.4488@gmail.com>
2022-04-21 11:45:00 +0200

    start with mongo

---
+++ README.md
@@ -0,0 +1,3 @@
+## MongoBD Demo
+
+training on MongoDB for school where I study to be a Front End developers
--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/playground')
+.then(() => console.log('connected'))
+.catch(err => console.error('Could not connect to MongoDB',err));
+
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean
+});
+
+const Course = mongoose.model('Course', courseSchema);
+
